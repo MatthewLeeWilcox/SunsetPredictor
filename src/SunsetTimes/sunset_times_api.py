1662bb067fc78cb9fb2ca5a3d9b2b3af7c64fd3a
anutabutsko <anutabutsko@gmail.com>
2024-04-23 11:37:43 -0400

    data for golden, co

--- src/SunsetTimes/sunset_times_api.py
+++ src/SunsetTimes/sunset_times_api.py
@@ -4,13 +4,14 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 
-def generate_dates(year):
-    start_date = datetime(year, 1, 1)
-    end_date = datetime(year, 12, 31)
+def generate_dates(start_year, end_year=2020):
+    start_date = datetime(start_year, 7, 18)
+    end_date = datetime(end_year, 2, 2)
 
     date_list = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range((end_date - start_date).days + 1)]
 
     return date_list
+
 
 def convert_utc_to_edt(utc_time_str, timezone):
     utc_time = datetime.strptime(utc_time_str, '%Y-%m-%dT%H:%M:%S+00:00')
@@ -31,38 +32,31 @@ def get_sunset_time(latitude, longitude, date):
     # Build the API request URL
     url = f"https://api.sunrise-sunset.org/json?lat={latitude}&lng={longitude}&date={date}&formatted=0"
 
-
     # Make the API request
     response = requests.get(url)
     data = response.json()
 
     # Extract the sunset time
     sunset = data['results']['sunset']
-    
+
     return sunset
 
 
-locations = {"Bondville, IL": [40.1134, -88.3695, 'America/Chicago'],
-             "Desert Rock, NV": [36.621, -116.029, 'America/Los_Angeles'],
-             "Fort Peck, MT": [ 48.0056, -106.4483, 'America/Denver'], 
-             "Goodwin Creek, MS": [],
-             "Sioux Falls, SD": [],
-             "State College, PA": [],
-             "Boulder, CO": []}
+location = {"Golden, CO": [39.7555, -105.2211, 'America/Denver']}
 
-for year in range(2006, 2025, 1):
+for year in range(2005, 2021, 1):
     dates = generate_dates(year)
 
     columns = ['Date', 'Sunset Time']
     df_sunset_times = pd.DataFrame(columns=columns)
 
     for date in dates:
-        sunset_time = get_sunset_time(locations["Desert Rock, NV"][0], locations["Desert Rock, NV"][1], date)
-        
-        local_time = convert_utc_to_edt(sunset_time, 'America/Los_Angeles')
+        sunset_time = get_sunset_time(location["Golden, CO"][0], location["Golden, CO"][1], date)
 
-        date_time = {'Date': date, 'Sunset Time': local_time} 
+        local_time = convert_utc_to_edt(sunset_time, location["Golden, CO"][2])
+
+        date_time = {'Date': date, 'Sunset Time': local_time}
         df_sunset_times.loc[len(df_sunset_times)] = date_time
         print(date)
 
-    df_sunset_times.to_csv(f'data/desert_rock/{year}.csv')
+    df_sunset_times.to_csv(f'data/golden_co/year_{year}.csv')
