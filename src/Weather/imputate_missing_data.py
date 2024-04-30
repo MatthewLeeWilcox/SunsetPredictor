c24593654307b44ae6b1d48b29bc00d454d787bd
anutabutsko <anutabutsko@gmail.com>
2024-04-29 20:51:51 -0400

    added date column

--- src/Weather/imputate_missing_data.py
+++ src/Weather/imputate_missing_data.py
@@ -8,5 +8,6 @@ df['date'] = pd.to_datetime(df['date'])
 df.set_index('date', inplace=True)
 
 df.interpolate(method='linear', inplace=True)
+df.reset_index(inplace=True)
 
-df.to_csv('src/Weather/data/golden_co/weather_data_interpolated.csv', index=False)
+df.to_csv('src/Weather/data/golden_co/weather_data_interpolated.csv', index=True)
