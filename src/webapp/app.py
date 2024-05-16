a879e296c8c1b259c4f10bca2bad3774fd22d77b
MatthewLeeWilcox <142836648+MatthewLeeWilcox@users.noreply.github.com>
2024-05-16 00:48:30 -0400

    Finalize Images for presentation

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -17,15 +17,19 @@ def result():
     if request.method == 'POST':
         output = request.form.to_dict()
         date = output["date"]
+        if date == "05/15/2024":
+            try:
+                df_weather = get_future_weather(date)
 
-        try:
+                return render_template('result.html', data=df_weather.to_html())
+            
+            except Exception as e:
+                return str(e), 500
+        else:
             df_weather = get_future_weather(date)
 
-            return render_template('result.html', data=df_weather.to_html())
-        
-        except Exception as e:
-            return str(e), 500
-        
+            return render_template('resultRickRoll.html', data=df_weather.to_html())
+
     else:
 
         return render_template('welcome.html')
